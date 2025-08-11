zkzk-blog-challenge/blog-challenge

[feat] 태그 기능 추가

.vitepress/theme/functions.ts
@@ -1,0 +1,60 @@
+type Post = {
+    frontMatter: {
+        date: string
+        title: string
+        category: string
+        tags: string[]
+        description: string
+    }
+    regularPath: string
+}
+
+export function initTags(posts: Post[]): Record<string, Post[]> {
+    const data: Record<string, Post[]> = {}
+    posts.forEach((post) => {
+        post.frontMatter.tags?.forEach((tag) => {
+            data[tag] = data[tag] || []
+            data[tag].push(post)
+        })
+    })
+
+    return Object.fromEntries(Object.entries(data).sort(([, posts1], [, posts2]) => posts2.length - posts1.length))
+}
+
+export function initCategory(posts: Post[]) {
+    const data: Record<string, Post[]> = {}
+    for (let index = 0; index < posts.length; index++) {
+        const element = posts[index]
+        const category = element.frontMatter.category
+        if (category) {
+            if (data[category]) {
+                data[category].push(element)
+            } else {
+                data[category] = []
+                data[category].push(element)
+            }
+        }
+    }
+    return data
+}
+
+export function useYearSort(post: Post[]) {
+    const data: Post[][] = []
+    let year = '0'
+    let num = -1
+    for (let index = 0; index < post.length; index++) {
+        const element = post[index]
+        if (element.frontMatter.date) {
+            const y = element.frontMatter.date.split('-')[0]
+            if (y === year) {
+                data[num].push(element)
+            } else {
+                num++
+                data[num] = []
+                data[num].push(element)
+                year = y
+            }
+        }
+    }
+    return data
+}

.vitepress/theme/index.ts
@@ -1,0 +1,25 @@
+import DefaultTheme from "vitepress/theme";
+import type { Theme } from "vitepress";
+import BaseLayout from "./BaseLayout.vue";
+import Archives from "./components/Archives.vue";
+import BlogArchives from "./components/BlogArchives.vue";
+import Category from "./components/Category.vue";
+import Tags from "./components/Tags.vue";
+import Pages from "./components/Pages.vue";
+import Comment from "./components/UtterancComment.vue";
+
+import "./style.css";   // style 파일은 custom.css 등 네이밍만 다를 뿐, 자유롭게
+
+export default {
+  ...DefaultTheme,
+  Layout: BaseLayout,
+  enhanceApp({ app }) {
+    // 글로벌 컴포넌트 등록
+    app.component("Tags", Tags);
+    app.component("Category", Category);
+    app.component("Archives", Archives);
+    app.component("BlogArchives", BlogArchives);
+    app.component("Pages", Pages);
+    app.component("Comment", Comment);
+  },
+} satisfies Theme;

.vitepress/theme/serverUtils.ts
@@ -1,0 +1,114 @@
+import { globby } from 'globby'
+import matter from 'gray-matter'
+import fs from 'fs-extra'
+import { resolve, join } from 'path'
+import { forceString } from './date'
+
+async function getPosts(pageSize: number) {
+    const isProd = process.env.NODE_ENV === 'production'
+    const ignorePaths = isProd ? ['posts/draft/**/*.md', 'posts/trash/**/*.md'] : []
+
+    let paths = await globby(['posts/**/**.md'], {
+        ignore: ignorePaths
+    })
+
+    // 여기서는 페이징을 생성할 때만 사용됨
+    // await generatePaginationPages(paths.length, pageSize)
+
+    let posts = await Promise.all(
+        paths.map(async (item) => {
+            const content = await fs.readFile(item, 'utf-8')
+            const { data } = matter(content)
+            return {
+                frontMatter: {
+                    ...data,
+                    date: forceString(data.date),
+                    // order 처리: 숫자가 아닐 경우 0으로 강제 변환
+                    order: _convertOrder(data.order)
+                },
+                regularPath: `/${item.replace('.md', '.html')}`
+            }
+        })
+    )
+    posts.sort(_compareDate as any)
+    return posts
+}
+
+async function generatePaginationPages(total: number, pageSize: number) {
+    // 전체 페이지 수 계산
+    let pagesNum = total % pageSize === 0 ? total / pageSize : Math.floor(total / pageSize) + 1
+    const paths = resolve('./')
+
+    if (total > 0) {
+        for (let i = 1; i < pagesNum + 1; i++) {
+            const page = `
+                    ---
+                    page: true
+                    title: ${i === 1 ? 'home' : 'page_' + i}
+                    aside: false
+                    comment: false
+                    ---
+                    <script setup>
+                    import Page from "./.vitepress/theme/components/Page.vue";
+                    import { useData } from "vitepress";
+                    const { theme } = useData();
+                    const posts = theme.value.posts.slice(${pageSize * (i - 1)},${pageSize * i})
+                    </script>
+                    <Page :posts="posts" :pageCurrent="${i}" :pagesNum="${pagesNum}" />
+                `.trim()
+
+            const file = paths + `/page_${i}.md`
+            await fs.writeFile(file, page)
+        }
+    }
+    // page_1을 index로 이름 변경하여 홈페이지로 사용
+    await fs.move(paths + '/page_1.md', paths + '/index.md', { overwrite: true })
+}
+
+// YYYY-MM-DD 일때의 코드
+// function _compareDate(
+//     obj1: { frontMatter: { date: number; order: number } },
+//     obj2: { frontMatter: { date: number; order: number } }
+// ) {
+//     const orderCompare = obj2.frontMatter.order - obj1.frontMatter.order
+//     if (orderCompare !== 0) return orderCompare
+//     return obj1.frontMatter.date < obj2.frontMatter.date ? 1 : -1
+// }
+
+function _compareDate(
+    obj1: { frontMatter: { date: string; order: number } },
+    obj2: { frontMatter: { date: string; order: number } }
+) {
+    const orderCompare = obj2.frontMatter.order - obj1.frontMatter.order
+    if (orderCompare !== 0) return orderCompare
+
+    // "YYYY년 QQ기 WW주차" → "YYYY-QQ-WW" 형태로 변환하여 비교
+    const weekKey1 = _parseYearQuarterWeek(obj1.frontMatter.date)
+    const weekKey2 = _parseYearQuarterWeek(obj2.frontMatter.date)
+
+    // 주차(연도+기수+주차) 비교
+    if (weekKey1 !== weekKey2) {
+        return weekKey1 < weekKey2 ? 1 : -1
+    }
+
+    // 같은 주차면 동일 그룹으로 간주 → 순서 그대로
+    return 0
+}
+
+function _parseYearQuarterWeek(dateStr: string): string {
+    // 예: "2025년 01기 03주차"
+    const match = dateStr.match(/(\d{4})년\s*(\d{1,2})기\s*(\d{1,2})주차/)
+    if (!match) return dateStr // 형식이 맞지 않으면 원본 반환
+    const [, year, quarter, week] = match
+    // 비교 용도로 YYYYQQWW 숫자 문자열 생성
+    return `${year.padStart(4, '0')}${quarter.padStart(2, '0')}${week.padStart(2, '0')}`
+}
+
+function _convertOrder(input?: unknown): number {
+    if (input === undefined || input === null) return 0
+    if (typeof input === 'number') return input
+    const num = Number(input)
+    return isNaN(num) ? 0 : num
+}
+
+export { getPosts }

writeNow-at/writeNow-at.github.io

.vitepress/theme/functions.ts
@@ -1,0 +1,60 @@
+type Post = {
+    frontMatter: {
+        date: string
+        title: string
+        author: string
+        tags: string[]
+        description: string
+    }
+    regularPath: string
+}
+
+export function initTags(posts: Post[]): Record<string, Post[]> {
+    const data: Record<string, Post[]> = {}
+    posts.forEach((post) => {
+        post.frontMatter.tags?.forEach((tag) => {
+            data[tag] = data[tag] || []
+            data[tag].push(post)
+        })
+    })
+
+    return Object.fromEntries(Object.entries(data).sort(([, posts1], [, posts2]) => posts2.length - posts1.length))
+}
+
+export function initAuthor(posts: Post[]) {
+    const data: Record<string, Post[]> = {}
+    for (let index = 0; index < posts.length; index++) {
+        const element = posts[index]
+        const author = element.frontMatter.author
+        if (author) {
+            if (data[author]) {
+                data[author].push(element)
+            } else {
+                data[author] = []
+                data[author].push(element)
+            }
+        }
+    }
+    return data
+}
+
+export function useYearSort(post: Post[]) {
+    const data: Post[][] = []
+    let year = '0'
+    let num = -1
+    for (let index = 0; index < post.length; index++) {
+        const element = post[index]
+        if (element.frontMatter.date) {
+            const y = element.frontMatter.date.split('-')[0]
+            if (y === year) {
+                data[num].push(element)
+            } else {
+                num++
+                data[num] = []
+                data[num].push(element)
+                year = y
+            }
+        }
+    }
+    return data
+}

.vitepress/theme/index.ts
@@ -1,0 +1,27 @@
+import DefaultTheme from "vitepress/theme";
+import type { Theme } from "vitepress";
+import BaseLayout from "./BaseLayout.vue";
+import Archives from "./components/Archives.vue";
+import BlogArchives from "./components/BlogArchives.vue";
+import Author from "./components/Author.vue";
+import Tags from "./components/Tags.vue";
+import Pages from "./components/Pages.vue";
+import Comment from "./components/UtterancComment.vue";
+import RecentPosts from "./components/RecentPosts.vue";
+
+import "./style.css";   // style 파일은 custom.css 등 네이밍만 다를 뿐, 자유롭게
+
+export default {
+  ...DefaultTheme,
+  Layout: BaseLayout,
+  enhanceApp({ app }) {
+    // 글로벌 컴포넌트 등록
+    app.component("Tags", Tags);
+    app.component("Author", Author);
+    app.component("Archives", Archives);
+    app.component("BlogArchives", BlogArchives);
+    app.component("Pages", Pages);
+    app.component("Comment", Comment);
+    app.component("RecentPosts", RecentPosts);
+  },
+} satisfies Theme;

.vitepress/theme/serverUtils.ts
@@ -1,0 +1,114 @@
+import { globby } from 'globby'
+import matter from 'gray-matter'
+import fs from 'fs-extra'
+import { resolve, join } from 'path'
+import { forceString } from './date'
+
+async function getPosts(pageSize: number) {
+    const isProd = process.env.NODE_ENV === 'production'
+    const IGNORE_GLOBS = isProd ? ['posts/draft/**/*.md', 'posts/trash/**/*.md'] : []
+
+    let paths = await globby(['posts/**/**.md'], {
+        ignore: IGNORE_GLOBS
+    })
+
+    // 여기서는 페이징을 생성할 때만 사용됨
+    // await generatePaginationPages(paths.length, pageSize)
+
+    let posts = await Promise.all(
+        paths.map(async (item) => {
+            const content = await fs.readFile(item, 'utf-8')
+            const { data } = matter(content)
+            return {
+                frontMatter: {
+                    ...data,
+                    date: forceString(data.date),
+                    // order 처리: 숫자가 아닐 경우 0으로 강제 변환
+                    order: _convertOrder(data.order)
+                },
+                regularPath: `/${item.replace('.md', '.html')}`
+            }
+        })
+    )
+    posts.sort(_compareDate as any)
+    return posts
+}
+
+async function generatePaginationPages(total: number, pageSize: number) {
+    // 전체 페이지 수 계산
+    let pagesNum = total % pageSize === 0 ? total / pageSize : Math.floor(total / pageSize) + 1
+    const paths = resolve('./')
+
+    if (total > 0) {
+        for (let i = 1; i < pagesNum + 1; i++) {
+            const page = `
+                    ---
+                    page: true
+                    title: ${i === 1 ? 'home' : 'page_' + i}
+                    aside: false
+                    comment: false
+                    ---
+                    <script setup>
+                    import Page from "./.vitepress/theme/components/Page.vue";
+                    import { useData } from "vitepress";
+                    const { theme } = useData();
+                    const posts = theme.value.posts.slice(${pageSize * (i - 1)},${pageSize * i})
+                    </script>
+                    <Page :posts="posts" :pageCurrent="${i}" :pagesNum="${pagesNum}" />
+                `.trim()
+
+            const file = paths + `/page_${i}.md`
+            await fs.writeFile(file, page)
+        }
+    }
+    // page_1을 index로 이름 변경하여 홈페이지로 사용
+    await fs.move(paths + '/page_1.md', paths + '/index.md', { overwrite: true })
+}
+
+// YYYY-MM-DD 일때의 코드
+// function _compareDate(
+//     obj1: { frontMatter: { date: number; order: number } },
+//     obj2: { frontMatter: { date: number; order: number } }
+// ) {
+//     const orderCompare = obj2.frontMatter.order - obj1.frontMatter.order
+//     if (orderCompare !== 0) return orderCompare
+//     return obj1.frontMatter.date < obj2.frontMatter.date ? 1 : -1
+// }
+
+function _compareDate(
+    obj1: { frontMatter: { date: string; order: number } },
+    obj2: { frontMatter: { date: string; order: number } }
+) {
+    const orderCompare = obj2.frontMatter.order - obj1.frontMatter.order
+    if (orderCompare !== 0) return orderCompare
+
+    // "YYYY년 QQ기 WW주차" → "YYYY-QQ-WW" 형태로 변환하여 비교
+    const weekKey1 = _parseYearQuarterWeek(obj1.frontMatter.date)
+    const weekKey2 = _parseYearQuarterWeek(obj2.frontMatter.date)
+
+    // 주차(연도+기수+주차) 비교
+    if (weekKey1 !== weekKey2) {
+        return weekKey1 < weekKey2 ? 1 : -1
+    }
+
+    // 같은 주차면 동일 그룹으로 간주 → 순서 그대로
+    return 0
+}
+
+function _parseYearQuarterWeek(dateStr: string): string {
+    // 예: "2025년 01기 03주차"
+    const match = dateStr.match(/(\d{4})년\s*(\d{1,2})기\s*(\d{1,2})주차/)
+    if (!match) return dateStr // 형식이 맞지 않으면 원본 반환
+    const [, year, quarter, week] = match
+    // 비교 용도로 YYYYQQWW 숫자 문자열 생성
+    return `${year.padStart(4, '0')}${quarter.padStart(2, '0')}${week.padStart(2, '0')}`
+}
+
+function _convertOrder(input?: unknown): number {
+    if (input === undefined || input === null) return 0
+    if (typeof input === 'number') return input
+    const num = Number(input)
+    return isNaN(num) ? 0 : num
+}
+
+export { getPosts }